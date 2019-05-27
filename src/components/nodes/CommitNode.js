c047a7f8dfbf114d3d62b652102c6037372f801e
ericyan <eric7578@gmail.com>
2019-05-27 17:41:48 +0800

    Fixed misused Pjax link

--- src/components/nodes/CommitNode.js
+++ src/components/nodes/CommitNode.js
@@ -1,14 +1,13 @@
 import React, {useContext} from 'react';
 import {DataSource} from '../../context';
-import Pjax from '../Pjax';
 
 const CommitNode = props => {
   const dataSource = useContext(DataSource);
   return (
     <>
-      <Pjax to={dataSource.getCommitNodePath(props, dataSource)}>
+      <a href={dataSource.getCommitNodePath(props, dataSource)}>
         {props.path}
-      </Pjax>
+      </a>
       <span>{`+${props.additions} -${props.deletions}`}</span>
     </>
   );
